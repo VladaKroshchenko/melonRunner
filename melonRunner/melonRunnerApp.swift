7b3774bc815cd0cbf03e6fbe82fe764e7030e119
Vlada Kroshchenko <v.kroshchenko@vk.team>
2025-07-29 12:00:26 +0300

    Initial Commit

---
+++ melonRunner/melonRunnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  melonRunnerApp.swift
+//  melonRunner
+//
+//  Created by Kroshchenko Vlada on 29.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct melonRunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ melonRunnerTests/melonRunnerTests.swift
@@ -0,0 +1,16 @@
+//
+//  melonRunnerTests.swift
+//  melonRunnerTests
+//
+//  Created by Kroshchenko Vlada on 29.07.2025.
+//
+
+import Testing
+
+struct melonRunnerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
